24aaf7baa387ff845c93632ae0832b0ddb1ece0e
kytaylor <ky.taylor5468@gmail.com>
2021-02-20 12:04:33 -0800

    adds initial controllers, adds more to orm.js

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const connection = require('./connection.js');
+
+const orm = {
+    selectAll: function(callback) {
+
+    },
+
+    insertOne: function(burger_name, callback) {
+
+    },
+
+    updateOne: function(burgerID, callback) {
+
+    }
+}
